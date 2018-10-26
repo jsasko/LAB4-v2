5049b16f1c9d87d2819e0ac4345a18cd91935a4b
jsasko <28355@pwsztar.edu.pl>
2018-10-26 18:10:58 +0200

    Do dokończenia

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -5,16 +5,44 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BlogItemComponent } from './blog-item/blog-item.component';
+import { BlogItemTextComponent } from './blog-item-text/blog-item-text.component';
+import { BlogItemImageComponent } from './blog-item-image/blog-item-image.component';
+import { BlogItemDetailComponent } from './blog-item-detail/blog-item-detail.component';
+import { SummaryPipe } from './summary.pipe';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'blog', component: BlogComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    QuizComponent
+    QuizComponent,
+    HomeComponent,
+    BlogComponent,
+    NavbarComponent,
+    BlogItemComponent,
+    BlogItemTextComponent,
+    BlogItemImageComponent,
+    BlogItemDetailComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ blog/src/app/blog-item-detail/blog-item-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-item-detail',
+  templateUrl: './blog-item-detail.component.html',
+  styleUrls: ['./blog-item-detail.component.scss']
+})
+export class BlogItemDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
